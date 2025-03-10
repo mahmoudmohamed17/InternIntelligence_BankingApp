7dc3fefceb2ce3d2b96b4d18cc3589449cc03b31
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-10 09:48:19 +0200

    modify app logo and splash

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -5,5 +5,9 @@ class Assets {
   /// Assets for imagesOnboarding
   /// assets/images/onboarding.svg
   static const String imagesOnboarding = "assets/images/onboarding.svg";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
 }
 
