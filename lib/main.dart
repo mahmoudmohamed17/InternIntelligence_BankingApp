56d40b0d63e3c26534f7a41a98f326047f121c91
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-10 08:34:55 +0200

    add needed assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:banking_app/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        tabBarTheme: TextTheme()
-      ),
+      theme: ThemeData(textTheme: AppTheme.textTheme),
       home: Scaffold(),
     );
   }
---
+++ lib/utils/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesOnboarding
+  /// assets/images/onboarding.svg
+  static const String imagesOnboarding = "assets/images/onboarding.svg";
+}
+
